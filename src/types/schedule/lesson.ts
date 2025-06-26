a0f683e2df77b88ee5469b53b5a5333aa9abb1d2
FakeMan2332 <oworld69@gmail.com>
2025-06-26 05:14:14 +0500

    feat: обновлены настройки для tsup

--- src/types/schedule/lesson.ts
+++ src/types/schedule/lesson.ts
@@ -1,6 +1,7 @@
-import type { Cabinet } from '@types/schedule/cabinet';
-import type { ScheduleTeacher } from '@types/schedule/index';
-import type { Timetable } from '@types/schedule/timetable';
+import type { Group } from '@/types/group';
+import type { Timetable } from '@/types/schedule/timetable';
+import type { ScheduleTeacher } from '@/types/schedule/index';
+import type { Cabinet } from '@/types/schedule/cabinet';
 
 export type Lesson = {
   number: number;
